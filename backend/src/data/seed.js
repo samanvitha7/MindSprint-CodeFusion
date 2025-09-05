31703cdd268711523119546a458c8dc9ca3d816d
sam <bolisettysamanvitha@gmail.com>
2025-09-06 02:51:02 +0530

    data seeded successfully

--- backend/src/data/seed.js
+++ backend/src/data/seed.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import Facility from "./models/facilityModel.js";
+import Facility from "../models/facilityModel.js";
 
 dotenv.config();
 
