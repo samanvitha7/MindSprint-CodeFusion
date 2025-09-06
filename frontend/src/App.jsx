6d028e6ba2483ada7fbb64045ea8159bd0abbb4d
Neel-aiprog <mhaskeneel0709@gmail.com>
2025-09-06 14:12:10 +0530

    integrated map

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import About from "./pages/About";
+import MapPage from "./pages/MapPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/about" element={<About />} />
+      <Route path="/map" element={<MapPage />} />
     </Routes>
   );
 }
