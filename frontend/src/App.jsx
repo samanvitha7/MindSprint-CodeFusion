6c4871721fe993b939a033b3f74b36a57d9a7199
sam <bolisettysamanvitha@gmail.com>
2025-09-06 13:13:16 +0530

    know maore page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import HomePage from "./pages/HomePage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import KnowMore from "./pages/KnowMore";
 
 function App() {
   return (
