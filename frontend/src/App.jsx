4920412d2eec6d24288a4fef52545dfb2704ddb7
naina <jainnaina1502@gmail.com>
2025-09-06 23:21:33 +0530

    about form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,38 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom"; // ✅ No BrowserRouter here
 
 import HomePage from "./pages/HomePage";
-
 import KnowMore from "./pages/KnowMore";
 import About from "./pages/About";
 import MapPage from "./pages/MapPage";
 
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/map" element={<MapPage />} />
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/map" element={<MapPage />} />
+        <Route path="/knowmore" element={<KnowMore />} />
+      </Routes>
 
-      <Route path="/knowmore" element={<KnowMore />} />
-
-    </Routes>
+      {/* ✅ Toast container available everywhere */}
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+    </>
   );
 }
 
