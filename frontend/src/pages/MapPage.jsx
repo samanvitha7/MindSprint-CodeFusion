9329468b7fd85b6fafa18727fe77f2d0a872045a
naina <jainnaina1502@gmail.com>
2025-09-07 02:00:35 +0530

    map

--- frontend/src/pages/MapPage.jsx
+++ frontend/src/pages/MapPage.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import Header from "../components/Header.jsx";
 import Map from "../components/Map.jsx";
 import Footer from "../components/Footer.jsx";
+
 const MapPage = () => {
   return (
-    <div className="min-h-screen bg-soft">
+    <div className="min-h-screen bg-soft flex flex-col">
       <Header />
-      <Map />
+      <main className="flex-1">
+        <Map />
+      </main>
       <Footer />
     </div>
   );
