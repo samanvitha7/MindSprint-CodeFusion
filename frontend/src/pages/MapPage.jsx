b9bad1bdcacad91c94c2ff25f0c0463c3f36ff96
Neel-aiprog <mhaskeneel0709@gmail.com>
2025-09-06 14:12:10 +0530

    integrated map

---
+++ frontend/src/pages/MapPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Header";
+import Map from "../components/Map";
+
+const MapPage = () => {
+  return (
+    <div className="min-h-screen bg-soft">
+      <Header />
+      <Map />
+    </div>
+  );
+};
+
+export default MapPage;
